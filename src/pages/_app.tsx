5027c1dc0f843b4ae71c7604c64f2f4d662bb485
fcaramez <francisco.caramez.duarte@gmail.com>
2023-01-18 21:58:45 +0000

    adds chakraUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/app";
 
 import { api } from "../utils/api";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
