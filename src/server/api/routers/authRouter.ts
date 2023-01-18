2a90bd7df66bc2c8c4530898fa76dec464cd47cd
fcaramez <francisco.caramez.duarte@gmail.com>
2023-01-18 21:56:43 +0000

    adds login functionality

--- src/server/api/routers/authRouter.ts
+++ src/server/api/routers/authRouter.ts
@@ -11,13 +11,12 @@ export const authRouter = createTRPCRouter({
     .input(z.object({ name: z.string(), password: z.string() }))
     .mutation(
       async ({
-        input,
+        input: { name, password },
       }): Promise<{
         message: string;
         success: boolean;
         data: Record<string, any> | null;
       }> => {
-        const { name, password } = input;
         try {
           if (!name || !password) {
             return {
@@ -54,15 +53,17 @@ export const authRouter = createTRPCRouter({
               numberOfGuesses: true,
               password: false,
               id: true,
+              isAdmin: true,
             },
           });
 
-          const { name: userName, numberOfGuesses, id } = newUser;
+          const { name: userName, numberOfGuesses, id, isAdmin } = newUser;
 
           const payload = {
             userName,
             numberOfGuesses,
             id,
+            isAdmin,
           };
 
           const authToken = jwt.sign(payload, TOKEN_SECRET as string, {
@@ -82,6 +83,78 @@ export const authRouter = createTRPCRouter({
           return {
             success: false,
             message: "Internal server error, call Xico",
+            data: null,
+          };
+        }
+      }
+    ),
+  login: publicProcedure
+    .input(z.object({ name: z.string(), password: z.string() }))
+    .mutation(
+      async ({
+        input: { name, password },
+      }): Promise<{
+        message: string;
+        success: boolean;
+        data: Record<string, any> | null;
+      }> => {
+        try {
+          if (!name || !password) {
+            return {
+              message: "All fields are mandatory!",
+              success: false,
+              data: null,
+            };
+          }
+
+          const userToFind = await prisma.player.findFirst({
+            where: {
+              name: name,
+            },
+          });
+
+          if (!userToFind) {
+            return {
+              success: false,
+              message: "User does not exist!",
+              data: null,
+            };
+          }
+
+          const verify = await argon.verify(userToFind.password, password);
+
+          if (!verify) {
+            return {
+              success: false,
+              message: "Wrong credentials!",
+              data: null,
+            };
+          }
+
+          const payload = {
+            name: userToFind.name,
+            numberOfGuesses: userToFind.numberOfGuesses,
+            id: userToFind.id,
+            isAdmin: userToFind.isAdmin,
+          };
+
+          const token = jwt.sign(payload, TOKEN_SECRET as string, {
+            algorithm: "HS256",
+            expiresIn: "7d",
+          });
+
+          return {
+            success: true,
+            message: `Welcome back ${name}`,
+            data: {
+              token: token,
+              ...payload,
+            },
+          };
+        } catch (_e) {
+          return {
+            success: false,
+            message: "Internal Server Error, call Xico",
             data: null,
           };
         }
